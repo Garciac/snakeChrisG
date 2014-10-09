f9ad179ee42532dccd920fd124f034b05738a3f7
Chris Garcia <chrisg1245@hotmail.com>
2014-10-09 14:00:07 -0700

    finish doing the background, finished milestone 15

--- public_html/js/Snake.js
+++ public_html/js/Snake.js
@@ -1,2 +1,42 @@
+var snake;
+var snakeLength;
+var snakeSize;
 
+var context;
+var screenWidth;
+var screenHeight;
 
+gameInitialize();
+gameDraw();
+
+function gameInitialize() {
+  var canvas = document.getElementById("game-screen");
+  context = canvas.getContext("2d");
+  
+  screenWidth = window.innerWidth;
+  screenHeight = window.innerHeight;
+  
+  canvas.width = screenWidth;
+  canvas.height = screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle = "rgb(30, 199, 199)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
+
+function snakeInitialize() {
+    snake = []; 
+}
+
+function snakeDraw() {
+    
+}
+
+function snakeUpdate() {
+    
+}
