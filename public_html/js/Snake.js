2f54a9a081966c2e833324a3f48b908ed9082085
Chris Garcia <chrisg1245@hotmail.com>
2014-10-22 14:02:16 -0700

    added l&d

--- public_html/js/Snake.js
+++ public_html/js/Snake.js
@@ -88,7 +88,7 @@ function snakeUpdate() {
         snakeHeadY++;
     }
     
-    else{
+    else if(snakeDirection == "right") {
         snakeHeadX++;
     }
     
@@ -113,17 +113,29 @@ function foodInitialize() {
 
 function foodDraw() {
     context.filStyle = "white";
-    context.fillRect(food.x, food.y, snakeSize, snakeSize);
+    context.fillRect(food.x * snakeSize, food.y * snakeSize, snakeSize);
 }
 
 function setFoodPosition() {
     var randomX = Math.floor(Math.random() * screenWidth);
     var randomY = Math.floor(Math.random() * screenHeight);
     
-    food.x = randomX;
-    food.y = randomY;
+    food.x = Math.floor(randomX / snakeSize);
+    food.y = Math.floor(randomY / snakeSize);
 }
+/*-----------------------------
+ * Input Functions
+ * ----------------------------
+ */
 
 function keyboardHandler(event) {
     console.log(event);
+    
+    if(event.keyCode =="39"){
+     snakeDirection = "right";
+ }
+ 
+    else if(event.keyCode =="40"){
+    snakeDirection = "down" ;
+  }
 }
